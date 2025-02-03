a939ce88a58cf1c7c8f0f02512fd17b021d8057c
Theo <ojw1912@gmail.com>
2025-02-03 11:30:59 +0900

    250203 11:30 # 1-3-2. 컴포넌트 만들기

---
+++ src/components/Bye.jsx
@@ -0,0 +1,17 @@
+// import React from "react"; // 리액트 18버전부터 생략 가능
+
+
+// 함수형 컴포넌트 : 최근 리액트 선호방식
+// rafce
+
+import React from 'react'
+
+
+const Bye = () => {
+  return (
+    <div>Bye</div>
+  )
+
+}
+
+export default Bye
---
+++ src/components/Example.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const Example = () => {
+  return (
+    <div>
+    </div>
+  );
+};
+export default Example;
