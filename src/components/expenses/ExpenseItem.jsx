30f8dc26f4e6d77149c259bb416b7089dbfd30c2
Theo <ojw1912@gmail.com>
2025-02-03 16:22:20 +0900

    250203 12:50 # 1-4-4. props로 객체 전달하기

--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -36,14 +36,19 @@ import ExpenseDate from './ExpenseDate';
 //   );
 // }
 
-const ExpenseItem = ({date, title, price}) => {
+const ExpenseItem = ({ expense }) => {
+  // console.log('props: ', props);
+  const { title, price, date } = expense;
+
+  // 원화 표기법으로 변환
+  const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
 
   return (
     <div className='expense-item'>
       <ExpenseDate expenseDate={date} />
       <div className='expense-item__description'>
         <h2>{title}</h2>
-        <div className='expense-item__price'>{price}원</div>
+        <div className='expense-item__price'>{formatPrice}원</div>
       </div>
     </div>
   );
---
+++ src/components/expenses/ExpenseList.jsx
@@ -0,0 +1,14 @@
+// rafc
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import './ExpenseList.css';
+const ExpenseList = ({ expenses }) => {
+  return (
+    <div className='expenses'>
+      <ExpenseItem expense={expenses[0]} />
+      <ExpenseItem expense={expenses[1]} />
+      <ExpenseItem expense={expenses[2]} />
+    </div>
+  );
+};
+export default ExpenseList;
