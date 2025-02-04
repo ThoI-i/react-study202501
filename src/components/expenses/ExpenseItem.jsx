6cd1e98e84b62bc96813e2f5e57233ed3b3a4635
Theo <ojw1912@gmail.com>
2025-02-04 10:16:40 +0900

    250204 10:15 # 2-2-1. useState 기초

--- src/components/expenses/ExpenseItem.jsx
+++ src/components/expenses/ExpenseItem.jsx
@@ -1,70 +1,50 @@
-// rafce
-import React from 'react'
-import './ExpenseItem.css'
+import React, { useState } from 'react';
+import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../ui/Card';
-
-// const ExpenseItem = (props) => {
-//   // console.log('props: ', props);
-  
-//   //렌더링에 필요한 변수 → 1단계 1번 밖에 사용되지 않음: 변수 생성X 바로 입력
-//   // const expenseDate = props.date;
-//   // const expenseTitle = props.title;
-//   // const expensePrice  = props.price;
-
-//   return (
-//     <div className='expense-item'>
-//       <div>{ props.date.toLocaleString() } </div>
-//       <div className='expense-item__description'>
-//         <h2>{props.title}</h2>
-//         <div className='expense-item__price'>{props.price}원</div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// 2단계: 디스턱처링
-// const ExpenseItem = ({date, title, price}) => {
-
-//   return (
-//     <div className='expense-item'>
-//       <div>{ date.toLocaleString() } </div>s
-//       <div className='expense-item__description'>
-//         <h2>{title}</h2>
-//         <div className='expense-item__price'>{price}원</div>
-//       </div>
-//     </div>
-//   );
-// }
 
 const ExpenseItem = ({ expense }) => {
   // console.log('props: ', props);
   const { title, price, date } = expense;
 
+  // 상태값을 처리하는 함수(hook)
+  /*
+    useState훅의 리턴값은 배열이며
+    첫번째 요소는 관리할 상태값의 초기값
+    두번째 요소는 해당 상태값을 변경할 때 사용하는 setter함수
+  */
+  let [itemTitle, setItemTitle] = useState(title);
+  // [itemTitle, setItemTitle] itemTitle 원본 | setItemTitle 변경할 값
+  // ㄴ>React의 Re-렌더링 변경 부분만 바꿈
+
+  console.log('itemTitle:', itemTitle);
+
   // 원화 표기법으로 변환
   const formatPrice = new Intl.NumberFormat('ko-KR').format(price);
 
   // 이벤트 핸들러 정의
-  function handleClick(e){
-    // console.log('click!!');
-    // console.log(e.target);
-        
+  function handleClick(e) {
+    /*
+      리액트는 단순히 변수가 변경된다고 화면의 렌더링을 변경하지 않고
+      오로지 상태값의 변경에 의해서만 렌더링을 변경
+    */
+    // itemTitle = '짜장면';
+    setItemTitle('짜장면');
   }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate expenseDate={date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{itemTitle}</h2>
         <div className='expense-item__price'>{formatPrice}원</div>
       </div>
 
-      <button id='btn1' onClick={handleClick}>버튼1</button>
-      <button id='btn2' onClick={e => alert('이벤트!!')}>버튼2</button>
-      <button id='btn2' onMouseOver={e => alert('이벤트!!')}>버튼3</button>
-      <button id='btn2' onDoubleClick={e => alert('이벤트!!')}>버튼4</button>
-      <button id='btn2' onMouseLeave={e => alert('이벤트!!')}>버튼5</button>
-
+      <button
+        id='btn1'
+        onClick={handleClick}>
+        수정
+      </button>
     </Card>
   );
 };
