7bd0704eb5a07a5c40331356855839ccb233d584
Theo <ojw1912@gmail.com>
2025-02-05 12:21:15 +0900

    250205 12:15 # 종합복습 1-1. 목표설정 앱 템플릿

---
+++ src/components/CourseGoals/CourseList.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './CourseList.css';
+import CourseItem from './CourseItem';
+const CourseList = () => {
+  return (
+    <ul className='goal-list'>
+      <CourseItem />
+      <CourseItem />
+      <CourseItem />
+    </ul>
+  );
+};
+export default CourseList;
