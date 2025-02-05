62b358b6e1dbcddccecc577ea5edf0351494a942
Theo <ojw1912@gmail.com>
2025-02-05 10:56:23 +0900

    250205 10:50 # 3-2-3. 차트 UI 생성

--- src/components/expenses/ExpenseList.jsx
+++ src/components/expenses/ExpenseList.jsx
@@ -1,8 +1,8 @@
-
 import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './ExpenseList.css';
 import ExpenseFilter from './ExpenseFilter';
+import ExpenseChart from '../chart/ExpenseChart';
 
 const ExpenseList = ({ expenses }) => {
 
@@ -11,21 +11,22 @@ const ExpenseList = ({ expenses }) => {
   // ExpenseFilter에서 선택한 연도값을 여기서 출력
   // 연도를 끌어올리기 위한 함수
   const onFilterChange = (filteredYear) => { 
-
+    
     console.log(`선택된 끌어올려진 연도: ${filteredYear}`);
     setYear(filteredYear);
   };
 
-
+  
 
   // const createItemArray = () => { 
   //   // expenses라는 객체배열을 ExpenseItem컴포넌트 배열로 변환
-
+    
   //   return expenses.map(ex => <ExpenseItem expense={ex} />);
   // };
 
   // 지출 데이터가 없을 때 보여줄 컴포넌트
   let content = <p>아직 해당년도의 지출항목이 없습니다.</p>;
+
   // 지출 데이터가 있을 때 보여줄 컴포넌트
   const filteredExpenses = expenses
     .filter((ex) => ex.date.getFullYear().toString() === year);
@@ -38,10 +39,11 @@ const ExpenseList = ({ expenses }) => {
       />
     ));
   }
-
+  
   return (
     <div className='expenses'>
       <ExpenseFilter onChangeFilter={onFilterChange} />
+      <ExpenseChart />
       { content }
     </div>
   );
