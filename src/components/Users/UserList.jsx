b56f9fd99bc414a558bab7f98514727d79681eec
Theo <ojw1912@gmail.com>
2025-02-07 10:27:16 +0900

    250207 10:26 # 5-2-1. 사용자 관리 템플릿

---
+++ src/components/Users/UserList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from './UserList.module.css';
+import Card from '../uI/Card';
+
+const UserList = () => {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        
+      </ul>
+    </Card>
+  );
+};
+
+export default UserList;
