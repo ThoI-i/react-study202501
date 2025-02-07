4974c226935a90f708ae8db47e31838674beb6fa
Theo <ojw1912@gmail.com>
2025-02-07 09:44:58 +0900

    250207 09:42 # 종합복습 2-4. 할 일 추가 기능(구현 완료)

--- src/components/Todos/TodoInput.jsx
+++ src/components/Todos/TodoInput.jsx
@@ -1,9 +1,10 @@
+
 import React, { useState } from 'react';
 import { MdAdd } from 'react-icons/md';
 
 import styles from './scss/TodoInput.module.scss';
 
-const TodoInput = () => {
+const TodoInput = ({onAdd}) => {
   const {
     'form-wrapper': wrapper,
     'insert-form': insertForm,
@@ -14,11 +15,20 @@ const TodoInput = () => {
   // 입력창을 열어줄지 여부를 관리
   const [open, setOpen] = useState(false);
 
+  // 할 일 제목을 상태관리
+  const [enteredTitle, setEnteredTitle] = useState('');
+
+  // 할 일 제목 입력 이벤트
+  const handleTitleInput = e => { 
+    setEnteredTitle(e.target.value);
+  };
+
   // 할 일 추가 이벤트
   const handleSubmit = e => { 
     e.preventDefault();
-    console.log('submit!');
     
+    onAdd(enteredTitle);
+    setEnteredTitle('');
   };
 
   return (
@@ -29,6 +39,8 @@ const TodoInput = () => {
             <input
               type='text'
               placeholder='할 일을 입력 후, 엔터를 누르세요!'
+              onInput={handleTitleInput}
+              value={enteredTitle}
             />
           </form>
         </div>
