cca3ee9c49d634d5b3cd9cc69696c3a9d3af48dd
Theo <ojw1912@gmail.com>
2025-02-07 11:16:38 +0900

    250207 11:16 # 5-2-4-A. 에러 모달 닫기 리펙토링

--- src/components/ui/Modal/ErrorModal.jsx
+++ src/components/ui/Modal/ErrorModal.jsx
@@ -3,21 +3,37 @@ import Card from '../Card';
 import Button from '../Button';
 import styles from './ErrorModal.module.css';
 
+// 백드롭 컴포넌트
+const BackDrop = ({ onClose }) => {
+  return (
+    <div
+      className={styles.backdrop}
+      onClick={onClose}></div>
+  );
+};
+
+// 모달 컴포넌트
+const ModalContent = ({ title, message, onClose }) => { 
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={onClose}>Okay</Button>
+      </footer>
+    </Card>
+  );
+};
+
 const ErrorModal = ({ title, message, onClose }) => {
   return (
     <>
-      <div className={styles.backdrop} onClick={onClose}></div>
-      <Card className={styles.modal}>
-        <header className={styles.header}>
-          <h2>{title}</h2>
-        </header>
-        <div className={styles.content}>
-          <p>{message}</p>
-        </div>
-        <footer className={styles.actions}>
-          <Button onClick={onClose}>Okay</Button>
-        </footer>
-      </Card>
+      <BackDrop onClose={onClose} />
+      <ModalContent title={title} message={message} onClose={onClose} />
     </>
   );
 };
