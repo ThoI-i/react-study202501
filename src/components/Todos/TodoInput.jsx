9c4383ae366e7b9ca36c8a667e81d87740251095
Theo <ojw1912@gmail.com>
2025-02-07 09:36:46 +0900

    250207 09:29 # 종합복습 2-4-0. 할 일 이벤트(Submit 기능)

--- src/components/Todos/TodoInput.jsx
+++ src/components/Todos/TodoInput.jsx
@@ -8,26 +8,35 @@ const TodoInput = () => {
     'form-wrapper': wrapper,
     'insert-form': insertForm,
     'insert-btn': insertBtn,
-    open: openStyle
+    open: openStyle,
   } = styles;
 
   // 입력창을 열어줄지 여부를 관리
   const [open, setOpen] = useState(false);
 
+  // 할 일 추가 이벤트
+  const handleSubmit = e => { 
+    e.preventDefault();
+    console.log('submit!');
+    
+  };
+
   return (
     <>
-      { 
-        open &&
+      {open && (
         <div className={wrapper}>
-          <form className={insertForm}>
+          <form className={insertForm} onSubmit={handleSubmit}>
             <input
               type='text'
               placeholder='할 일을 입력 후, 엔터를 누르세요!'
             />
           </form>
         </div>
-      }
-      <button className={`${insertBtn} ${open ? openStyle : ''}`} onClick={() => setOpen(!open)}>
+      )}
+      <button
+        className={`${insertBtn} ${open ? openStyle : ''}`}
+        onClick={() => setOpen(!open)}
+      >
         <MdAdd />
       </button>
     </>
