0e42a966e23ca1c884be20e1b36c8cb3dbf35b49
Theo <ojw1912@gmail.com>
2025-02-07 12:44:25 +0900

    250207 12:43 # 6-1-1-C. side effect ID/PW 임의 검증 → 새로고침 시 초기화됨

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,27 @@ import Login from './components/SideEffect/Login';
 // 컴포넌트
 const App = () => {
 
+  // 로그인 상태 체크
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  // 로그인 수행 함수를 전달
+  const handleLogin = (email, password) => { 
+
+    // 검증
+    if (email === 'abc@def.com' && password === '12345678') {
+      localStorage.setItem('login-flag', '1');
+      setIsLoggedIn(true);
+    } else {
+      alert('로그인 실패!');
+    }
+  };
+
   return <>
-  <MainHeader />
-  <main>
-    {/* <Home /> */}
-    <Login />
-  </main>
+    <MainHeader />
+    <main>
+      {isLoggedIn && <Home />}
+      {!isLoggedIn && <Login onLogin={handleLogin} />}
+    </main>
   </>;
 };
 
--- src/components/SideEffect/Login.jsx
+++ src/components/SideEffect/Login.jsx
@@ -4,7 +4,7 @@ import Card from '../ui/Card';
 import styles from './Login.module.css';
 import Button from '../ui/Button';
 
-const Login = () => {
+const Login = ({ onLogin }) => {
   // 사용자가 입력한 이메일을 상태관리
   const [enteredEmail, setEnteredEmail] = useState('');
 
@@ -48,9 +48,15 @@ const Login = () => {
     setPasswordIsValid(enteredPassword.trim().length > 6);
   };
 
+  // 로그인 수행 함수
+  const handleSubmit = e => { 
+    e.preventDefault();
+    onLogin(enteredEmail, enteredPassword);
+  };
+
   return (
     <Card className={styles.login}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div
           className={`${styles.control} ${emailIsValid === false ? styles.invalid : ''}`}>
           <label htmlFor='email'>E-Mail</label>
