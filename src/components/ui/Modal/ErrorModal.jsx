81561aec60b24073dfc1f8a4d5dad6c935f2d95a
Theo <ojw1912@gmail.com>
2025-02-07 11:25:10 +0900

    250207 11:23 # 5-2-5. React Portal 기본

--- src/components/ui/Modal/ErrorModal.jsx
+++ src/components/ui/Modal/ErrorModal.jsx
@@ -3,6 +3,9 @@ import Card from '../Card';
 import Button from '../Button';
 import styles from './ErrorModal.module.css';
 
+// portal을 사용하기 위한 
+import ReactDOM from 'react-dom';
+
 // 백드롭 컴포넌트
 const BackDrop = ({ onClose }) => {
   return (
@@ -32,8 +35,19 @@ const ModalContent = ({ title, message, onClose }) => {
 const ErrorModal = ({ title, message, onClose }) => {
   return (
     <>
-      <BackDrop onClose={onClose} />
-      <ModalContent title={title} message={message} onClose={onClose} />
+      {ReactDOM.createPortal(
+        <BackDrop onClose={onClose} />,
+        document.getElementById('backdrop-root')
+      )}
+
+      {ReactDOM.createPortal(
+        <ModalContent
+          title={title}
+          message={message}
+          onClose={onClose}
+        />,
+        document.getElementById('overlay-root')
+      )}
     </>
   );
 };
