de242971488c7df92ca0b65db070c47f343917d6
Theo <ojw1912@gmail.com>
2025-02-07 09:12:51 +0900

    250207 09:11 # 종합복습 2-2-A. form toggle !open

--- src/components/Todos/TodoInput.jsx
+++ src/components/Todos/TodoInput.jsx
@@ -1,21 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { MdAdd } from 'react-icons/md';
+
 import styles from './scss/TodoInput.module.scss';
+
 const TodoInput = () => {
+  const {
+    'form-wrapper': wrapper,
+    'insert-form': insertForm,
+    'insert-btn': insertBtn,
+  } = styles;
+
+  // 입력창을 열어줄지 여부를 관리
+  const [open, setOpen] = useState(false);
+
   return (
     <>
-      <div className='form-wrapper'>
-        <form className='insert-form'>
-          <input
-            type='text'
-            placeholder='할 일을 입력 후, 엔터를 누르세요!'
-          />
-        </form>
-      </div>
-      <button className='insert-btn'>
+      { 
+        open &&
+        <div className={wrapper}>
+          <form className={insertForm}>
+            <input
+              type='text'
+              placeholder='할 일을 입력 후, 엔터를 누르세요!'
+            />
+          </form>
+        </div>
+      }
+      <button className={insertBtn} onClick={() => setOpen(!open)}>
         <MdAdd />
       </button>
     </>
   );
 };
+
 export default TodoInput;
