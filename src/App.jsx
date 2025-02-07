50c62e86954d9dfc87d483d2a9218abd254502a1
Theo <ojw1912@gmail.com>
2025-02-07 12:50:06 +0900

    250207 12:50 # 6-1-1-1. side effect 리-렌더링으로 인한 무한 루프 발생

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,19 @@ import Login from './components/SideEffect/Login';
 // 컴포넌트
 const App = () => {
 
+
   // 로그인 상태 체크
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+
+  // 지금 이 컴포넌트가 렌더링될 때 localStorage에서 토큰을 꺼내서 이전에 로그인 했던 사람인지를 확인
+  console.log('토큰 검사!!');
+  const token = localStorage.getItem('login-flag');
+
+  if (token === '1') {
+    setIsLoggedIn(true);
+  }
+  
 
   // 로그인 수행 함수를 전달
   const handleLogin = (email, password) => { 
