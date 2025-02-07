2de3df98b44c2af9498833f59ef38838799c8c37
Theo <ojw1912@gmail.com>
2025-02-07 09:23:37 +0900

    250207 09:23 # 종합복습 2-3-0. 반복문 미사용

--- src/components/Todos/TodoMain.jsx
+++ src/components/Todos/TodoMain.jsx
@@ -1,15 +1,25 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './scss/TodoMain.module.scss';
 import TodoItem from './TodoItem';
 
+
+const DUMMY_TODOS = [
+  { id: 1, title: '리액트 공부', done: true },
+  { id: 2, title: '점심 먹기', done: false },
+  { id: 3, title: '프로젝트하기', done: false },
+  { id: 4, title: '숙제하기', done: false },
+];
+
+// 컴포넌트
 const TodoMain = () => {
+  
   return (
     <ul className={styles['todo-list']}>
-      <TodoItem />
-      <TodoItem />
-      <TodoItem />
+      <TodoItem item={DUMMY_TODOS[0]} />
+      <TodoItem item={DUMMY_TODOS[1]} />
+      <TodoItem item={DUMMY_TODOS[2]} />
+      <TodoItem item={DUMMY_TODOS[3]} />
     </ul>
   );
 };
