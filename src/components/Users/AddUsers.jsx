e59025b506dffc91e972a0ba4f3d02d35e3816e5
Theo <ojw1912@gmail.com>
2025-02-07 10:54:07 +0900

    250207 10:53 # 5-2-3. 에러 모달 띄우기

--- src/components/Users/AddUsers.jsx
+++ src/components/Users/AddUsers.jsx
@@ -11,6 +11,10 @@ const AddUsers = ({ onAddUser }) => {
     age: '',
   });
 
+  // 에러가 났을 때 에러의 데이터를 관리할 상태변수
+  // error -> { title: '에러 제목', message: '에러의 내용' }
+  const [error, setError] = useState(null);
+
   const handleName = (e) => {
     setUserValue({
       ...userValue,
@@ -27,6 +31,22 @@ const AddUsers = ({ onAddUser }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
 
+    // 입력값 검증
+    if (!userValue.username.trim()) {
+      setError({
+        title: '유효하지 않은 입력값',
+        message: '입력값은 필수값입니다.'
+      });
+      return;
+    }
+    if (+userValue.age < 1) {
+      setError({
+        title: '유효하지 않은 나이 범위',
+        message: '나이는 0이나 음수가 될 수 없습니다.',
+      });
+      return;
+    }
+
     onAddUser({
       ...userValue,
       id: Math.random().toString(),
@@ -40,7 +60,7 @@ const AddUsers = ({ onAddUser }) => {
 
   return (
     <>
-      <ErrorModal title={'모달 제목'} message={'모델 내용'} />
+      {error && <ErrorModal title={error.title} message={error.message} />}
       <Card className={styles.input}>
         <form onSubmit={handleSubmit}>
           <label htmlFor='username'>이름</label>
